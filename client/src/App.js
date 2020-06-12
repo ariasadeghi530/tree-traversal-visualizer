2fa64360793a6c91a2dbcc9e34693f23214441fb
Aria Sadeghi <ariasadeghi@gmail.com>
2020-06-11 21:16:53 -0700

    backend flask server and react frontend working

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,9 @@ import React from 'react';
 function App() {
   return (
     <>
-    <h1>
-      Hello, World!
-    </h1>
+      <h1>Hello World!</h1>
     </>
-    );
+  );
 }
 
 export default App;
